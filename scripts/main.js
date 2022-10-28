48a432816945e037146ccd3bbbc90b22f3b8066c
hypescord <hypescord@gmail.com>
2022-10-28 17:26:10 +0100

    my first commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let menu = false
+function toggle() {
+    if(menu == false) {
+        document.getElementById("one").classList.add("active");
+        menu = true    
+        console.log("clicked");
+    } else {
+        document.getElementById("one").classList.remove("active");
+        menu = false
+    }
+}    
